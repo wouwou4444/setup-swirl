100d9c108af8a5dffdcbd46f4645f5fb09d23d0e
wouwou4444 <valery.buz@outlook.com>
2018-10-20 13:29:29 +0200

    Add swirl installer for courses 2 and 3

--- swirl_setup.R
+++ swirl_setup.R
@@ -4,6 +4,10 @@ install.packages("swirl")
 packageVersion("swirl")
 library(swirl)
 
+install_from_swirl("R Programming")
+
+install_from_swirl("Getting and Cleaning Data")
+
 install_from_swirl("Exploratory Data Analysis")
 
 swirl()
